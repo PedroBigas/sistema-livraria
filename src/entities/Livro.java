b1887af03f98a7c97a3251d77e0f7ebe870db020
Pedro Bigas <pedrobbigas@gmail.com>
2025-10-13 23:18:00 -0300

    feat: criar class para livro

--- src/entities/Livro.java
+++ src/entities/Livro.java
@@ -1,4 +1,77 @@
 package entities;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Livro {
+    private Integer id;
+    private String titulo;
+    private Autor autor;
+    private Boolean disponivel;
+    private LocalDate dataCadastro;
+    private LocalDate dataAtualizacao;
+
+    public Livro(Integer id, String titulo, Autor autor, LocalDate dataCadastro) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Livro(Integer id, String titulo, Autor autor, Boolean disponivel, LocalDate dataCadastro) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = disponivel;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public LocalDate getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDate dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(id, livro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
