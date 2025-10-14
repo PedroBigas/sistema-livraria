a3d7f5930207c1262bf7d77fa97f731ce58d5a2a
Pedro Bigas <pedrobbigas@gmail.com>
2025-10-13 23:03:34 -0300

    feat: criar class para autor

--- src/entities/Autor.java
+++ src/entities/Autor.java
@@ -1,4 +1,44 @@
 package entities;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Autor {
+    private Integer id;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Autor(Integer id, String nome, LocalDate dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Autor autor = (Autor) o;
+        return Objects.equals(id, autor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
