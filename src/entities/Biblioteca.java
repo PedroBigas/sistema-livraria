4d6dc925a978db8f9180bf24e893cbc334b61ee3
Pedro Bigas <pedrobbigas@gmail.com>
2025-10-13 23:27:56 -0300

    feat: criar classe para biblioteca

--- src/entities/Biblioteca.java
+++ src/entities/Biblioteca.java
@@ -1,4 +1,28 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+    private List<Livro> livros = new ArrayList<>();
+    private List<Autor> autores = new ArrayList<>();
+    private List<Emprestimo> emprestimos = new ArrayList<>();
+
+    public Biblioteca(List<Livro> livros, List<Autor> autores, List<Emprestimo> emprestimos) {
+        this.livros = livros;
+        this.autores = autores;
+        this.emprestimos = emprestimos;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public List<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
 }
