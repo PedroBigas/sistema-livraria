6124dd69307cd8b5881913a37fb9119dcf3684b1
Pedro Bigas <pedrobbigas@gmail.com>
2025-10-13 23:25:03 -0300

    feat: criar class para emprestimo

---
+++ src/entities/Emprestimo.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Emprestimo {
+    private Integer id;
+    private Livro livro;
+    private String nomeCliente;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
+
+    public Emprestimo(Integer id, Livro livro, String nomeCliente, LocalDate dataEmprestimo, LocalDate dataDevolucao) {
+        this.id = id;
+        this.livro = livro;
+        this.nomeCliente = nomeCliente;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Emprestimo that = (Emprestimo) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
